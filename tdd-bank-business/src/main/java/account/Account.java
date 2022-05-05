a6571683b8c47fb12a6f9f49ae7d193ade62b0d2
Frankle97 <pairpapa@gmail.com>
2022-05-05 18:55:18 +0900

    [Bank Business] feat: Account 생성

---
+++ tdd-bank-business/src/main/java/account/Account.java
@@ -0,0 +1,14 @@
+package account;
+
+public class Account {
+
+    private final int balance;
+
+    public Account(int money) {
+        this.balance = money;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+}
