0b624906b225bd1551507cd5641c28b726625f4a
Frankle97 <pairpapa@gmail.com>
2022-05-05 13:44:29 +0900

    [Problem Solving] Inflearn - Array01

---
+++ problem-solving/src/main/java/lecture/inflearn/array/Array_01.java
@@ -0,0 +1,29 @@
+package lecture.inflearn.array;
+
+import java.util.Scanner;
+
+public class Array_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String[] s = sc.nextLine().split(" ");
+        int[] intArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = Integer.parseInt(s[i]);
+        }
+        System.out.println(solution1(intArray));
+    }
+
+    private static String solution1(int[] intArray) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(intArray[0]).append(" ");
+        for (int i = 1; i < intArray.length; i++) {
+            int prevNum = intArray[i - 1];
+            int currentNum = intArray[i];
+            if (currentNum >= prevNum) {
+                sb.append(currentNum).append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
