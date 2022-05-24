8fe4a2b0e1fb8b2b3e46003aee2980f79c83bf1f
Frankle97 <pairpapa@gmail.com>
2022-05-24 09:22:22 +0900

    [Problem Solving] Inflearn - Array09

---
+++ problem-solving/src/main/java/lecture/inflearn/array/Array_09.java
@@ -0,0 +1,56 @@
+package lecture.inflearn.array;
+
+import java.util.Scanner;
+
+public class Array_09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(solution1(n, arr));
+    }
+
+    private static int solution(int n, int[][] arr) {
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+            if (max < sum) {
+                max = sum;
+            }
+            sum = 0;
+
+            for (int k = 0; k < arr[i].length; k++) {
+                sum += arr[k][i];
+            }
+            if (max < sum) {
+                max = sum;
+            }
+            sum = 0;
+
+            for (int p = 0; p < arr[i].length; p++) {
+                sum += arr[p][p];
+            }
+            if (max < sum) {
+                max = sum;
+            }
+            sum = 0;
+
+            for (int u = n; u > arr[i].length; u--) {
+                sum += arr[u][u];
+            }
+            if (max < sum) {
+                max = sum;
+            }
+        }
+
+        return max;
+    }
+}
