ce4e8040cc920a9108e73701c1b93f86f2e42abc
Frankle97 <pairpapa@gmail.com>
2022-08-31 08:52:52 +0900

    [Tobi Spring] 1장. 오브젝트와 의존관계

---
+++ topring-example/src/main/java/springbook/Main.java
@@ -0,0 +1,26 @@
+package springbook;
+
+import java.sql.SQLException;
+import springbook.user.dao.NUserDao;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new NUserDao();
+        User user = new User();
+        user.setId("frankle");
+        user.setName("정재엽");
+        user.setPassword("1234");
+
+        userDao.add(user);
+
+        System.out.println("등록 성공, user.getId() = " + user.getId());
+
+        User user1 = userDao.get(user.getId());
+        System.out.println("user1 Name = " + user1.getName());
+        System.out.println("user1 Pass = " + user1.getPassword());
+    }
+
+}
