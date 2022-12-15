b0c58a9d2855d035f1750b16655b479306ad971e
Frankle97 <pairpapa@gmail.com>
2022-12-16 08:17:11 +0900

    test 1

--- java-code-manipulation/src/main/java/Masulsa.java
+++ java-code-manipulation/src/main/java/Masulsa.java
@@ -10,7 +10,7 @@ public class Masulsa {
         try {
             new ByteBuddy().redefine(Moja.class)
                     .method(ElementMatchers.named("pullOut")).intercept(FixedValue.value("Rabbit!"))
-                    .make().saveIn(new File("/Users/jaeyejung/IdeaProjects/frankle/code-kata/java-code-manipulation/target/classes/"));
+                    .make().saveIn(new File("/Users/bbb/IdeaProjects/frankle/code-kata/java-code-manipulation/target/classes/"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
