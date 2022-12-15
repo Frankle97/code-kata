ddd89d80b333b881d98030d523ec6b71d0bd5116
Frankle97 <pairpapa@gmail.com>
2022-12-16 08:09:23 +0900

    test

--- java-code-manipulation/src/main/java/Masulsa.java
+++ java-code-manipulation/src/main/java/Masulsa.java
@@ -1,12 +1,19 @@
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Masulsa {
     public static void main(String[] args) {
-//        try {
-//            new ByteBuddy().redefine(Moja.class)
-//                    .method(ElementMatchers.named("pullOut")).intercept(FixedValue.value("Rabbit!"))
-//                    .make().saveIn(new File("/Users/jaeyejung/IdeaProjects/frankle/code-kata/java-code-manipulation/target/classes/"));
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            new ByteBuddy().redefine(Moja.class)
+                    .method(ElementMatchers.named("pullOut")).intercept(FixedValue.value("Rabbit!"))
+                    .make().saveIn(new File("/Users/jaeyejung/IdeaProjects/frankle/code-kata/java-code-manipulation/target/classes/"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         System.out.println(new Moja().pullOut());
     }
