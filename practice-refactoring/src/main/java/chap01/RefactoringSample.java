f780ddbde8154c71f85ee0c9b9823dc0a4586533
Frankle97 <pairpapa@gmail.com>
2023-04-04 23:05:42 +0900

    refactor: amountFor 변수명 수정

--- practice-refactoring/src/main/java/chap01/RefactoringSample.java
+++ practice-refactoring/src/main/java/chap01/RefactoringSample.java
@@ -26,27 +26,27 @@ public class RefactoringSample {
         return result;
     }
 
-    private static int amountFor(Performance perf, Play play) throws Exception {
-        var thisAmount = 0;
+    private static int amountFor(Performance performance, Play play) throws Exception {
+        var result = 0;
 
         switch (play.getType()) {
             case "tragedy":
-                thisAmount = 40000;
-                if (perf.getAudience() > 30) {
-                    thisAmount += 1000 * (perf.getAudience() - 30);
+                result = 40000;
+                if (performance.getAudience() > 30) {
+                    result += 1000 * (performance.getAudience() - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
-                if (perf.getAudience() > 20) {
-                    thisAmount += 10000 + 500 * (perf.getAudience() - 20);
+                result = 30000;
+                if (performance.getAudience() > 20) {
+                    result += 10000 + 500 * (performance.getAudience() - 20);
                 }
-                thisAmount += 300 * perf.getAudience();
+                result += 300 * performance.getAudience();
                 break;
             default:
                 throw new Exception("알 수 없는 장르");
         }
-        return thisAmount;
+        return result;
     }
 
     public static void main(String[] args) throws Exception {
